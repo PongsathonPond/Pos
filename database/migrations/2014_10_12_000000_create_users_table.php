e3be43cf7294459d3bef8651cf79cd474d5a654a
pongsathon.l <pongsathon.l@touchtechnologies.co.th>
2023-01-27 15:05:50 +0700

    add migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,15 +24,100 @@ return new class extends Migration
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+
+
+
+
+        Schema::create('debtors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('debtor_id');
+            $table->string('description');
+            $table->decimal('amount', 10, 2);
+            $table->date('due_date');
+            $table->timestamps();
+            $table->foreign('debtor_id')->references('id')->on('debtors')->onDelete('cascade');
+        });
+
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('debt_id');
+            $table->decimal('amount', 10, 2);
+            $table->date('payment_date');
+            $table->timestamps();
+            $table->foreign('debt_id')->references('id')->on('debts')->onDelete('cascade');
+        });
+
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('debt_id');
+            $table->string('status');
+            $table->date('collection_date');
+            $table->timestamps();
+            $table->foreign('debt_id')->references('id')->on('debts')->onDelete('cascade');
+        });
+
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
+
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('order_product');
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('product_category');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('collections');
+        Schema::dropIfExists('payments');
+        Schema::dropIfExists('debts');
+        Schema::dropIfExists('debtors');
     }
 };
